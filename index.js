c150628f60394de08dd019b5d24a1bb3af3e25c0
omsi96 <omsi96@gmail.com>
2021-09-22 17:51:51 +0300

    Finishing first part

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * Part 1:
+ * 1. Define your name
+ * 2. Define your hobbies as a list
+ * 3. Push a new hobby
+ * 4. Pop the last hobby
+ * 5. Delete all hobbies
+ * 6. Get the length of hobbies
+ * 7. Slice the the array into half
+ * 8. Get an index of a specific hobby
+ * 9.
+ */
+
+// let name = "Omar Alibrahim";
+// ARRAY
+let hobbies = [
+  "Reading",
+  "Singing",
+  "Coding",
+  "Teaching",
+  "Walking",
+  "Reading",
+  "Running",
+];
+
+console.log("SLICE----");
+
+const sliced = hobbies.slice(2, 4);
+
+console.log(sliced);
+
+console.log(hobbies.indexOf("Reading"));
+
+/**
+ *
+ * Part 2: Movies DOM
+ */
+
+/**
+ *
+ * This function gets executed when you press on any delete button
+ * It gives you back the index
+ * Write the logic behind deleting a movie at specific index
+ */
